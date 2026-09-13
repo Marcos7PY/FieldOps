9e26c9190b336f7e1552aa85bde059218bd79b5e
Marcos <m.r.p.y17@gmail.com>
2026-09-13 01:52:30 -0500

    Añadir script de creación de bases de datos

---
+++ infra/sql/00_create_databases.sql
@@ -0,0 +1,4 @@
+﻿IF DB_ID('fieldops_auth') IS NULL CREATE DATABASE fieldops_auth;
+IF DB_ID('fieldops_orders') IS NULL CREATE DATABASE fieldops_orders;
+IF DB_ID('fieldops_notifications') IS NULL CREATE DATABASE fieldops_notifications;
+IF DB_ID('fieldops_analytics') IS NULL CREATE DATABASE fieldops_analytics;
