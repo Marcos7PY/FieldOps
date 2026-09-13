3967a28dd39d9a748d37778dfb162a314aadba6e
Marcos <m.r.p.y17@gmail.com>
2026-09-13 08:19:00 -0500

    Inicializar aplicación Ionic

---
+++ frontend/mobile-technician/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fieldops.technician',
+  appName: 'mobile-technician',
+  webDir: 'www'
+};
+
+export default config;
